b05fd5e52140b747bdf3c065c154b27753cd42a2
jac11 <jac11devel@gmail.com>
2024-02-21 16:43:04 -0500

    Add files via upload

--- Xor_Main.py
+++ Xor_Main.py
@@ -2,7 +2,7 @@
 
 import argparse
 import sys
-
+from Package.ErrorPrint import ErrorPrint
 class Main_Proecess:
  
     def __init__(self):
@@ -11,48 +11,52 @@ class Main_Proecess:
         self._Call_Code()
 
     def _Call_Code(self):
+        try:
+            
+            if self.args.exploit and not self.args.assembly:
 
-        if self.args.exploit and not self.args.assembly:
+                from Package.MSF_GET import _MSF_Call
+                _MSF_Call._Features(self,args1=self._arg_Parse)
 
-            from Package.MSF_GET import _MSF_Call
-            _MSF_Call._Features(self,args1=self._arg_Parse)
+                if self.args.load:
+                    from Package.RandomVar import RandomVar
+                    RandomVar.RandomVarCreate(self)  
+                    RandomVar.RandomName(self,argsN=self._arg_Parse) 
 
-            if self.args.load:
-                from Package.RandomVar import RandomVar
-                RandomVar.RandomVarCreate(self)  
-                RandomVar.RandomName(self,argsN=self._arg_Parse) 
+                from Package.MSF_GET import Xor_class
+                Xor_class.Gen_Key(self,args1=self._arg_Parse)
+                Xor_class.Xor_Process(self,args1=self._arg_Parse)
+                Xor_class.Fainal_Stage(self,args1=self._arg_Parse)
+              
 
-            from Package.MSF_GET import Xor_class
-            Xor_class.Gen_Key(self,args1=self._arg_Parse)
-            Xor_class.Xor_Process(self,args1=self._arg_Parse)
-            Xor_class.Fainal_Stage(self,args1=self._arg_Parse)
-          
+            elif self.args.assembly and self.args.exploit:
 
-        elif self.args.assembly and self.args.exploit:
+                from Package.assembly import ShellDump
+                ShellDump.CreateFile(self,args=self._arg_Parse)
+                ShellDump.LINKER_ASSEMBLY(self,args=self._arg_Parse)
+                ShellDump.ReplaceText(self,args=self._arg_Parse)
 
-            from Package.assembly import ShellDump
-            ShellDump.CreateFile(self,args=self._arg_Parse)
-            ShellDump.LINKER_ASSEMBLY(self,args=self._arg_Parse)
-            ShellDump.ReplaceText(self,args=self._arg_Parse)
+                from Package.MSF_GET import _MSF_Call
+                _MSF_Call._Features(self,args1=self._arg_Parse)
 
-            from Package.MSF_GET import _MSF_Call
-            _MSF_Call._Features(self,args1=self._arg_Parse)
+                if self.args.load:
+                   from Package.RandomVar import RandomVar
+                   RandomVar.RandomVarCreate(self)  
+                   RandomVar.RandomName(self,argsN=self._arg_Parse)  
 
-            if self.args.load:
-               from Package.RandomVar import RandomVar
-               RandomVar.RandomVarCreate(self)  
-               RandomVar.RandomName(self,argsN=self._arg_Parse)  
-
-            from Package.MSF_GET import Xor_class
-            Xor_class.Gen_Key(self,args1=self._arg_Parse)
-            Xor_class.Xor_Process(self,args1=self._arg_Parse)
-            Xor_class.Fainal_Stage(self,args1=self._arg_Parse)
-     
-
+                from Package.MSF_GET import Xor_class
+                Xor_class.Gen_Key(self,args1=self._arg_Parse)
+                Xor_class.Xor_Process(self,args1=self._arg_Parse)
+                Xor_class.Fainal_Stage(self,args1=self._arg_Parse)
+            ErrorPrint.INFO(self,args=self._arg_Parse)
+        except Exception as ErrorCommand:
+            self.ErrorCommand  = ErrorCommand        
+            ErrorPrint.Error(self,args=self.ErrorCommand)         
+      
     def _arg_Parse(self):
 
         parser = argparse.ArgumentParser(description="Usage: [OPtion] [arguments] [ -w ] [arguments]")       
-        parser.add_argument("-E","--exploit"    , action=None ,help ="Specify the Payload to be encrypted") 
+        parser.add_argument("-E","--exploit"    , action=None ,required=True,help ="Specify the Payload to be encrypted") 
         parser.add_argument("-B","--Key_base64" , action='store_true' ,default=False,help ="Use base64 encoding key value for encryption")  
         parser.add_argument("-b","--P_base64"   , action='store_true' ,default=False,help ="Use base64 encoding for the Payload Before decryption")      
         parser.add_argument("-O","--output"     ,action=None          ,required=True,help ="Specify the output file for the encrypted Payload") 
